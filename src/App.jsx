86f8052d503f0141a8227b58dac7eb344d52e082
Kartikey Raghav <kartikeyraghav2006@gmail.com>
2024-12-29 18:41:47 +0530

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import ButtonGradient from "../public/assets/svg/ButtonGradient";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+      </div>
+      <ButtonGradient />
+    </>
+  );
 };
 
 export default App;
